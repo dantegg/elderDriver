a3852e6ac3171fd4da6707c13c8ea8d2708ac2c2
dantegg <luroufan327@hotmail.com>
2016-11-29 22:07:57 +0800

    first test spider

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ rosioo.com
 
 ##dependence
 python,scrapy
+
+##reference
+[scrapy入门教程](http://scrapy-chs.readthedocs.io/zh_CN/0.24/intro/tutorial.html)
+
+##testscrapy.py
+a official demo from [scrapy.org](http://scrapy.org/)
---
+++ driver/driver/spiders/simple_spider.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class oldDriverSpider(scrapy.Spider):
+    name = 'oldDriver'
+    allowed_domains = ['rosiok.com']
+    start_urls=[
+        "http://www.rosiok.com/shipin/",
+        "http://www.rosiok.com/sishu/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        with open(filename,'wb') as f:
+            f.write(response.body)
